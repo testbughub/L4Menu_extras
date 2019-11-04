962b1cf77909f2501819258193a71774746d6de4
testbughub <andreas.lamarca@gmail.com>
2019-11-04 10:08:09 +0100

    ..

--- extras/SafeShutdown.py
+++ extras/SafeShutdown.py
@@ -10,7 +10,7 @@ power = LED(powerenPin)
 power.on()
 
 def when_pressed():
-  os.system("sudo pkill emulationstatio & openvt -c 1 -f clear && sudo omxplayer -b '/home/pi/RetroPie/splashscreens/RPz_off.mp4' && openvt -c 1 -f clear && sudo shutdown -h now")
+  os.system("sudo killall emulationstation & openvt -c 1 -f clear && sudo omxplayer -b '/home/pi/RetroPie/splashscreens/RPz_off.mp4' && openvt -c 1 -f clear && sudo shutdown -h now")
 
 btn = Button(powerPin, hold_time=hold)
 btn.when_pressed = when_pressed
